e2a023ed59bdec55e3f9ff0f99e376ba6140d08f
Danylo <d.fedorenko@istu.edu.ua>
2024-10-28 14:37:06 +0200

    chore(frontend): Set up API interceptors for request/response handling

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,5 +1,30 @@
-import axios from "axios";
+import axios, { isAxiosError } from "axios";
 
 export const api = axios.create({
 	baseURL: "http://localhost:3000/api",
 });
+
+api.interceptors.request.use((config) => {
+	// TODO: get session from storage
+	const session = { id: "temp", userId: "temp", expiresAt: "never" };
+	if (session) {
+		config.headers.Authorization = `Bearer ${session.id}`;
+	}
+
+	return config;
+});
+
+api.interceptors.response.use(
+	(response) => response,
+	(error) => {
+		if (
+			isAxiosError(error) &&
+			error.response &&
+			error.response.status === 401
+		) {
+			// TODO: redirect user to login page and remove token
+		}
+
+		return Promise.reject(error);
+	}
+);
